055fdbcfb88a93fb9275798d3970be7c4ba0fddc
Bot1101 <kapustianskyov@gmail.com>
2020-11-27 23:56:06 +0200

    Delete db.js

--- rent/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://Oleg:qwerty1234@cluster0.t6xiq.mongodb.net/db?retryWrites=true&w=majority";
-
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true
-})
